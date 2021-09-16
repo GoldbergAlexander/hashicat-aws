53805e8270fa499cd500dce8667dee769f4608cc
Your Name <you@example.com>
2021-09-16 20:02:03 +0000

    update s3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,7 +3,7 @@ module "s3_bucket" {
 
   bucket = "my-s3-bucket"
   acl    = "private"
-  bucket_prefix = "alexandergoldberg"
+#   bucket_prefix = "alexandergoldberg"
 
   versioning = {
     enabled = true
